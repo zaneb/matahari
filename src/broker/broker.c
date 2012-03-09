zaneb/matahari

commit 5c306455cc33ef9f01ff50dd7897af375572d549

Broker: rhbz#795430 - Ignore modules except for SSL
Don't load all available Qpid modules in the broker; only the SSL module.
This prevents newly-added modules (such as the Qpid store) being
automatically loaded. In that particular case, it was causing SELinux
issues.

Signed-off-by: Zane Bitter <zbitter@redhat.com>

src/broker/broker.c
@@ -85,6 +85,9 @@ static char **broker_args(int argc, char * const argv[])
     APPEND_ARG(newargs, o, portarg);
     APPEND_ARG(newargs, o, "--data-dir=" LOCAL_STATE_DIR "/lib/matahari");
 
+    APPEND_ARG(newargs, o, "--no-module-dir");
+    APPEND_ARG(newargs, o, "--load-module=" LIB_DIR "/qpid/daemon/ssl.so");
+
     while (i < argc) {
         APPEND_ARG(newargs, o, argv[i++]);
     }

src/include/config.h.in
@@ -11,6 +11,7 @@
 #cmakedefine HAVE_AUGEAS 1
 
 #define LOCAL_STATE_DIR "@localstatedir@"
+#define LIB_DIR         "@LIB_INSTALL_DIR@"
 
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE

commit fa22b631c8f4d37cf9149cbcffe43fc33a46f74d

Broker: rhbz#795430 - Fix buffer overflow
Introduced by 5c306455cc33ef9f01ff50dd7897af375572d549

Signed-off-by: Zane Bitter <zbitter@redhat.com>

src/broker/broker.c
@@ -70,7 +70,7 @@ static char **broker_args(int argc, char * const argv[])
 {
     int i = 0, o = 0;
     char portarg[13];
-    char **newargs = malloc(sizeof(char *) * (argc + 2 + 1));
+    char **newargs = malloc(sizeof(char *) * (argc + 4 + 1));
 
     if (!newargs) {
         mh_err("Failed to allocate argument list");